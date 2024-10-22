f65c9b3216f0652442d8b87fc7be91f3fa5bd7fd
DELL <DELL@HWANGLONGGG>
2024-10-22 07:54:08 +0700

    Add project files.

---
+++ KTCK_LTW/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KTCK_LTW.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult ProductList()
+        {
+            return View();
+        }
+        public ActionResult Toyota()
+        {
+            return View();
+        }
+        public ActionResult Honda()
+        {
+            return View();
+        }
+        public ActionResult BMW()
+        {
+            return View();
+        }
+        public ActionResult Ford()
+        {
+            return View();
+        }
+        public ActionResult Mercedes()
+        {
+            return View();
+        }
+        public ActionResult Hyundai()
+        {
+            return View();
+        }
+    }
+}
---
+++ KTCK_LTW/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KTCK_LTW.MvcApplication" Language="C#" %>
