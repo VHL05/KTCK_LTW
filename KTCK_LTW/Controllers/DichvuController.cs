e443331f2d37e09ef1e64fc59f6782cdb643b379
Võ Hoàng Long <23DH114438@st.huflit.edu.vn>
2024-10-24 08:36:46 +0700

    jihsgiwhugh

---
+++ KTCK_LTW/Controllers/DichvuController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KTCK_LTW.Controllers
+{
+    public class DichvuController : Controller
+    {
+        // GET: Dichvu
+        public ActionResult Laithu()
+        {
+            return View();
+        }
+        public ActionResult Datlichbaoduongxe()
+        {
+            return View();
+        }
+    }
+}
